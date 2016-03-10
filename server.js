4df37f38bb7e31299bfbcbf5719694b98aac7d72
zeek <zeek@localhost.localdomain>
2016-03-09 22:24:36 -0500

    Adding mustache server side templating and expressjs

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
-const express    = require('express');
-const app        = express();
-const port 	 = process.env.PORT || 8080;
+'use strict'
 
-const path       = require('path');
-const publicPath = path.resolve(__dirname, 'public');
+const express         = require('express');
+const app             = express();
+const port 	      = process.env.PORT || 8080;
 
+const path            = require('path');
+const publicPath      = path.resolve(__dirname, 'public');
+const mustacheExpress = require('mustache-express');
+
+// Configuration
 app.set('port', port);
 app.use(express.static(publicPath));
+
+app.engine('html', mustacheExpress());
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
 
 // Load the routes
 require('./routes/routes.js')(app);
 
 // Start server
 app.listen(port, function() {
- console.log('Server started on port: '+port); 
+ console.log('Server started on port: '+port);
 });
