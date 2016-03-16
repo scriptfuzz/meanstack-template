5a2caac30f1d4e670bbee8f6f5897fb515854fc1
zeek <zeek@localhost.localdomain>
2016-03-15 22:43:45 -0400

    Adding fake call to yelp api

--- yelp/yelp-client.js
+++ yelp/yelp-client.js
@@ -6,9 +6,31 @@ const http = require('http');
 
 module.exports = function() {
 
+ let options = {
+  host: 'https://api.yelp.com/',
+  path: '/v2/',
+  port: 80,
+  method: 'GET'
+ };
+
+ let callback = function(response) {
+
+   let result = '';
+
+   response.on('data', (chunk) => {
+     result += chunk;
+   });
+
+   response.on('end', () => {
+     console.log(result);
+     return result;
+   });
+ };
+
  return {
    search: (location) => { 
-    console.log('Seaching yelp for restaurants');
+     console.log('Seaching yelp for restaurants: '+location);
+     return http.request(options, callback);
    }
   };
 
