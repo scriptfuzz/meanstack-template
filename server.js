83c876a5c5a1cd617ab4f98ba19b07406d08a7de
josediaz30 <jose.diaz30@upr.edu>
2016-03-18 01:24:03 -0400

    Using single const statement

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
 'use strict';
 
 // Importing necesary libraries
-const express         = require('express');
-const app             = express();
-const port            = process.env.PORT || 8080;
-
-const path            = require('path');
-// Path to serve static web files (css, jss, etc.)
-const publicPath      = path.resolve(__dirname, 'public');
-const mustacheExpress = require('mustache-express');
+const express      = require('express'),
+   app             = express(),
+   port            = process.env.PORT || 8080,
+   path            = require('path'),
+   staticFileDir   = path.resolve(__dirname, 'public'),
+   mustacheExpress = require('mustache-express'),
+   apiVersion      = 'v1';
 
 // Configuration
 app.set('port', port);
-app.use(express.static(publicPath));
+app.use(express.static(staticFileDir));
 // Configure server-side templaing engine
 app.engine('html', mustacheExpress());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 
-const version = 'v1';
 // Load the API routes.
-require('./routes/api-routes.js')(app, version);
+require('./routes/api-routes.js')(app, apiVersion);
 
 // Load the web site routes.
 require('./routes/site-routes.js')(app);
