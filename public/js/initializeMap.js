711bcb3746f3c0e62057c09d9c5083cb97759d01
josediaz30 <jose.diaz30@upr.edu>
2016-03-21 23:45:46 -0400

    indentation fix

--- public/js/initializeMap.js
+++ public/js/initializeMap.js
@@ -4,21 +4,21 @@
 
 	function initialize() {
 		var mapProp = {
-	  	center: new google.maps.LatLng(51.508742,-0.120850),
-	    zoom:5,
-	    mapTypeId: google.maps.MapTypeId.ROADMAP
-	  };
-	  var map = new google.maps.Map(document.getElementById('googleMap'), mapProp);
-	};
+      center: new google.maps.LatLng(51.508742,-0.120850),
+      zoom:5,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById('googleMap'), mapProp);
+  };
 
-	google.maps.event.addDomListener(window, 'load', initialize);
+  google.maps.event.addDomListener(window, 'load', initialize);
 
-	if (navigator.geolocation) {
-	  location = navigator.geolocation.getCurrentPosition();
-	  console.log('location: '+location);
-	} else {
-	  console.log('Geolocation is not supported by this browser.');
-	}
+  if (navigator.geolocation) {
+   location = navigator.geolocation.getCurrentPosition();
+   console.log('location: '+location);
+ } else {
+   console.log('Geolocation is not supported by this browser.');
+ }
  
 })();
 
