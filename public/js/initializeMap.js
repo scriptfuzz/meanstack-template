9bed05d14a437eba47bc5064673b53517d754761
josediaz30 <jose.diaz30@upr.edu>
2016-03-22 00:40:03 -0400

    Reading geolocation

--- public/js/initializeMap.js
+++ public/js/initializeMap.js
@@ -1,26 +1,27 @@
 (function () {
+  'use strict';
 
-  var location = null;
+  var location = {};
   
   function initialize() {
     var mapProp = {
-      center: new google.maps.LatLng(51.508742,-0.120850),
+      center: new google.maps.LatLng(location.latitude, location.longitude),
       zoom:5,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     var map = new google.maps.Map(document.getElementById('googleMap'), mapProp);
   };
 
-  google.maps.event.addDomListener(window, 'load', initialize);
+  function getCoordinates(position) {
+    location = {latitude: position.coords.latitude, longitude: position.coords.longitude};
+    console.log('location: '+JSON.stringify(location));
+  };
 
   if (navigator.geolocation) {
-   location = navigator.geolocation.getCurrentPosition();
-   console.log('location: '+location);
+    navigator.geolocation.getCurrentPosition(getCoordinates);
+    google.maps.event.addDomListener(window, 'load', initialize);
   } else {
    console.log('Geolocation is not supported by this browser.');
   }
  
 })();
-
-
-
