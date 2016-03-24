672a0e708310965307e551e655bf26aa846268dd
josediaz30 <jose.diaz30@upr.edu>
2016-03-24 01:56:48 -0400

    coordinate attempt

--- public/js/coordinatesModule.js
+++ public/js/coordinatesModule.js
@@ -1,23 +1,26 @@
 // This module is responsible for storing the users
 // location in the COORDINATES global variable.
 
-var COORDINATES = (function () {
+var COORDINATES = (function() {
   'use strict';
 
   var location = {};
 
-  function getCoordinates(position) {
-    console.log('location: ' + JSON.stringify(location));
-    location = { latitude: position.coords.latitude, longitude: position.coords.longitude };
+  function assignCoordinates(coordinates) {
+    return coordinates;
   };
 
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(getCoordinates);
+    
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var c = {latitude: position.coords.latitude, longitude: position.coords.longitude};
+      console.log('position: ' + JSON.stringify(c));
+      assignCoordinates(c);
+    });
+
   } else {
     console.log('Geolocation is not supported by this browser.');
-    location = 'Could not getl location';
+    return location;
   }
-
-  return location;
 
 }());
