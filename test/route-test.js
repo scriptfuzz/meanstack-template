dedba0b1428480c8cfbeb489d8eb846d5553a89a
josediaz30 <jose.diaz30@upr.edu>
2016-04-05 23:37:37 -0400

    Fixing indentation

--- test/route-test.js
+++ test/route-test.js
@@ -5,30 +5,30 @@ const request = require('supertest');
 
 describe('loading express', function() {
 
-	// Instantiate the server
-	var server;
+  // Instantiate the server
+  var server;
 
-	beforeEach(function() {
-		// Remove cache
-		delete require.cache[require.resolve('../server.js')];
-		server = require('../server.js');
-	});
+  beforeEach(function() {
+  	// Remove cache
+  	delete require.cache[require.resolve('../server.js')];
+  	server = require('../server.js');
+  });
 
-	afterEach(function(done) {
-		server.close(done);
-	});
+  afterEach(function(done) {
+  	server.close(done);
+  });
 
-	it('responds to /', function testSlash(done) {
-		request(server)
-			.get('/')
-			.expect(200, done);
-	});
+  it('responds to /', function testSlash(done) {
+  	request(server)
+  		.get('/')
+  		.expect(200, done);
+  });
 
-	it('404 everything else', function testPath(done) {
-		console.log('test 404');
-		request(server)
-			.get('/foo/bar')
-			.expect(404, done);
-	});
+  it('404 everything else', function testPath(done) {
+  	console.log('test 404');
+  	request(server)
+  		.get('/foo/bar')
+  		.expect(404, done);
+  });
 
 });
