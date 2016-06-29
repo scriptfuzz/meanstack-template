2f1079262ec4b9943025adda4d386f07d8b62708
josediaz30 <jose.diaz30@upr.edu>
2016-06-28 22:33:49 -0400

    Adding oauth.sh

---
+++ oauth.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export CONSUMER_KEY='xxxxxxxx'
+																						 
+export CONSUMER_SECRET='xxxxxxxxxxxx'
+
+export TOKEN='xxxxxxxx'
+
+export TOKEN_SECRET='xxxxxx'
+
+export PORT=9000
